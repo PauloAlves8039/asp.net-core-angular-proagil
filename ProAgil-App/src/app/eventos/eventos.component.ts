0efa5003aaa4d014eb80e1d13693e270f972261a
PauloAlves8039 <pj38alves@gmail.com>
2020-10-16 20:48:29 -0300

    Configurando requisição HTTP

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -5,36 +5,36 @@
  * @version 1.0.1 (14/10/2020)
  */
 
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  styleUrls: ['./eventos.component.css'],
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any = [
-    {
-      EventoId: 1,
-      Tema: 'Angular',
-      Local: 'Belo Horizonte'
-    },
-    {
-      EventoId: 2,
-      Tema: '.NET Core',
-      Local: 'São Paulo'
-    },
-    {
-      EventoId: 3,
-      Tema: 'Angular e .NET Core',
-      Local: 'Rio de Janeiro'
-    }
-  ];
+  eventos: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getEventos();
   }
 
+  /**
+   * Função responsável por listar todos os eventos.
+   */
+  getEventos(): void {
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      (response) => {
+        this.eventos = response;
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
