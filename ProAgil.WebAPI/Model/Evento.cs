ad312cfd772276f09de0bcfa72562ab182685fcb
PauloAlves8039 <pj38alves@gmail.com>
2020-10-18 18:27:25 -0300

    Implementando novo campo para imagens

--- ProAgil.WebAPI/Model/Evento.cs
+++ ProAgil.WebAPI/Model/Evento.cs
@@ -11,5 +11,6 @@ namespace ProAgil.WebAPI.Model
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string Lote { get; set; }
+        public string ImagemURL { get; set; }
     }
 }
