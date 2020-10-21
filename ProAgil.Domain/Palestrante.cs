494a99b9ab679545195a65d116f2eddaddfbe785
PauloAlves8039 <pj38alves@gmail.com>
2020-10-20 21:27:46 -0300

    Implementação da entidade Palestrante

---
+++ ProAgil.Domain/Palestrante.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace ProAgil.Domain
+{
+    /// <summary>
+    /// Classe responsável pela atribuição das propriedades da entidade Palestrante.
+    /// </summary>
+    public class Palestrante
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string MiniCurriculo { get; set; }
+        public string ImagemURL { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public List<RedeSocial> RedesSociais { get; set; }
+    }
+}
