edcd1e0493f3ff7ccea6cd6d42e84b9ec649f58d
PauloAlves8039 <pj38alves@gmail.com>
2020-10-29 20:49:44 -0300

    Criando filtro de Data

---
+++ ProAgil-App/src/app/_helps/DateTimeFormatPipe.pipe.ts
@@ -0,0 +1,24 @@
+/**
+ * @file DateTimeFormatPipe.ts
+ * @author: Paulo Alves
+ * @description: pipe responsável pelo filtro de formatação do campo data.
+ * @version 1.0.1 (29/10/2020)
+ */
+
+import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Constants } from './../util/Constants';
+
+/**
+ * Classe responsável pela atribuição de pipe para filtragem do campo data.
+ */
+@Pipe({
+  name: 'DateTimeFormatPipe'
+})
+export class DateTimeFormatPipePipe extends DatePipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    return super.transform(value, Constants.DATE_TIME_FMT);
+  }
+
+}
---
+++ ProAgil-App/src/app/util/Constants.ts
@@ -0,0 +1,14 @@
+/**
+ * @file Constants.ts
+ * @author: Paulo Alves
+ * @description: responsável pelo atribuição de constantes utilitárias.
+ * @version 1.0.1 (29/10/2020)
+ */
+
+ /**
+  * Classe responsável por receber constantes para formação de campos.
+  */
+export class Constants {
+    static readonly DATE_FMT = 'dd/MM/yyyy';
+    static readonly DATE_TIME_FMT = `${Constants.DATE_FMT} hh:mm`;
+}
