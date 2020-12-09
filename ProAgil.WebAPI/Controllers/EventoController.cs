47de3d59130337b306410da661e5693d78af8caf
PauloAlves8039 <pj38alves@gmail.com>
2020-12-08 21:39:35 -0300

    Implementação inicial para adição de imagens

--- ProAgil.WebAPI/Controllers/EventoController.cs
+++ ProAgil.WebAPI/Controllers/EventoController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -47,7 +49,7 @@ namespace ProAgil.WebAPI.Controllers
             try
             {
                 var evento = await _repo.GetEventoAsyncById(EventoId, true);
-                
+
                 var results = _mapper.Map<EventoDto>(evento);
 
                 return Ok(results);
@@ -73,6 +75,36 @@ namespace ProAgil.WebAPI.Controllers
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao pesquisar registro: " + ex.Message);
             }
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName;
+                    var fullPath = Path.Combine(pathToSave, fileName.Replace("\"", " ").Trim());
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+
+                return Ok();
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao pesquisar registros: {ex.Message}");
+            }
+
+            return BadRequest("Erro oa tentar realizar upload!");
         }
 
         [HttpPost]
