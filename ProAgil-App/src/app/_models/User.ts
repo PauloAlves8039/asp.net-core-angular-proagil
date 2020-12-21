333981fb38d2159396419d4997e54a82e520a2bf
PauloAlves8039 <pj38alves@gmail.com>
2020-12-21 20:32:33 -0300

    Definindo a classe User

---
+++ ProAgil-App/src/app/_models/User.ts
@@ -0,0 +1,13 @@
+/**
+ * @file User.ts
+ * @author: Paulo Alves
+ * @description: classe responsável pela definição das propriedades do usuário.
+ * @version 1.0.1 (21/12/2020)
+ */
+
+export class User {
+  userName: string;
+  email: string;
+  password: string;
+  fullName: string;
+}
