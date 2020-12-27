162f554f6c2a4179d77cd05cfa8fe6a46a67892c
PauloAlves8039 <pj38alves@gmail.com>
2020-12-26 21:42:15 -0300

    Implementação inicial  para Form Groups Lote e Rede Social

--- ProAgil-App/src/app/_models/Evento.ts
+++ ProAgil-App/src/app/_models/Evento.ts
@@ -1,7 +1,7 @@
 /**
  * @file Evento.ts
  * @author: Paulo Alves
- * @description: interface responsável pela definição das propriedades da entidade Evento.
+ * @description: Classe responsável pela definição das propriedades da entidade Evento.
  * @version 1.0.1 (27/10/2020)
  */
 
@@ -10,18 +10,20 @@ import { RedeSocial } from './RedeSocial';
 import { Palestrante } from './Palestrante';
 
 /**
- * Interface para declaração das propriedades da entidade Evento.
+ * Classe para declaração das propriedades da entidade Evento.
  */
-export interface Evento {
-    id: number;
-    local: string;
-    dataEvento: Date;
-    tema: string;
-    qtdPessoas: number;
-    imagemURL: string;
-    telefone: string;
-    email: string;
-    lotes: Lote[];
-    redesSociais: RedeSocial[];
-    palestranteEventos: Palestrante[];
+export class Evento {
+  constructor() {}
+
+  id: number;
+  local: string;
+  dataEvento: Date;
+  tema: string;
+  qtdPessoas: number;
+  imagemURL: string;
+  telefone: string;
+  email: string;
+  lotes: Lote[];
+  redesSociais: RedeSocial[];
+  palestranteEventos: Palestrante[];
 }
--- ProAgil-App/src/app/eventos/evento-edit/evento-edit.component.ts
+++ ProAgil-App/src/app/eventos/evento-edit/evento-edit.component.ts
@@ -6,12 +6,13 @@
  */
 
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder } from '@angular/forms';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { ToastrService } from 'ngx-toastr';
 import { EventoService } from 'src/app/_services/evento.service';
 import { FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { Evento } from 'src/app/_models/Evento';
 
 @Component({
   selector: 'app-evento-edit',
@@ -20,8 +21,19 @@ import { Validators } from '@angular/forms';
 })
 export class EventoEditComponent implements OnInit {
   titulo = 'Editar Evento';
-  evento = {};
+  evento: Evento = new Evento();
+  imagemURL = 'assets/img/upload.png';
   registerForm: FormGroup;
+  file: File;
+
+  get lotes(): FormArray {
+    return <FormArray>this.registerForm.get('lotes');
+  }
+
+  get redesSociais(): FormArray {
+    return <FormArray>this.registerForm.get('redesSociais');
+  }
+
   constructor(
     private eventoService: EventoService,
     private fb: FormBuilder,
@@ -47,10 +59,43 @@ export class EventoEditComponent implements OnInit {
       ],
       local: ['', Validators.required],
       dataEvento: ['', Validators.required],
-      imagemURL: ['', Validators.required],
+      imagemURL: [''],
       qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
       telefone: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
+      lotes: this.fb.array([this.criaLote()]),
+      redesSociais: this.fb.array([this.criaRedeSocial()]),
     });
+  }
+
+  criaLote(): FormGroup {
+    return this.fb.group({
+      nome: ['', Validators.required],
+      quantidade: ['', Validators.required],
+      preco: ['', Validators.required],
+      dataInicio: [''],
+      dataFim: [''],
+    });
+  }
+
+  adicionarLote() {
+    this.lotes.push(this.criaLote());
+  }
+
+  adicionarRedeSocial() {
+    this.redesSociais.push(this.criaRedeSocial());
+  }
+
+  criaRedeSocial(): FormGroup {
+    return this.fb.group({
+      nome: ['', Validators.required],
+      url: ['', Validators.required],
+    });
+  }
+
+  onFileChange(file: FileList) {
+    const reader = new FileReader();
+    reader.onload = (event: any) => (this.imagemURL = event.target.result);
+    reader.readAsDataURL(file[0]);
   }
 }
