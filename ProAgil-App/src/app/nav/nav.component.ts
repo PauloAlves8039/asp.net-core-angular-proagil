04c41c3cd22682ae2d6b014a5cd7220e52cb9ae2
PauloAlves8039 <pj38alves@gmail.com>
2021-01-04 21:41:52 -0300

    Atualizando nome do usuário lohado

--- ProAgil-App/src/app/nav/nav.component.ts
+++ ProAgil-App/src/app/nav/nav.component.ts
@@ -37,4 +37,8 @@ export class NavComponent implements OnInit {
     this.toastr.show('Log Out');
     this.router.navigate(['/user/login']);
   }
+
+  userName() {
+    return sessionStorage.getItem('username');
+  }
 }
