bcb3dd0ef5d9f91f769aebd5b2549e6dab47f691
PauloAlves8039 <pj38alves@gmail.com>
2021-01-04 22:37:31 -0300

    Atulizando imagem de resgistro

--- ProAgil-App/src/app/_models/Lote.ts
+++ ProAgil-App/src/app/_models/Lote.ts
@@ -5,15 +5,15 @@
  * @version 1.0.1 (27/10/2020)
  */
 
- /**
-  * Interface para declaração das propriedades da entidade Lote.
-  */
+/**
+ * Interface para declaração das propriedades da entidade Lote.
+ */
 export interface Lote {
-    id: number;
-    nome: string;
-    preco: number;
-    dataInicio: Date;
-    dataFim: Date;
-    quantidade: number;
-    eventoId: number;
+  id: number;
+  nome: string;
+  preco: number;
+  dataInicio: Date;
+  dataFim: Date;
+  quantidade: number;
+  eventoId: number;
 }
